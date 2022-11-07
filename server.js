b8d7c506eb2a4cf994a8ba6e558cf0ecde264833
Jordan <jkwan104@gmail.com>
2022-11-07 14:47:55 -0800

    git ignore and server update

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_KEY);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (request, response) => {
+
+  response.status(200).send('Welcome to Our Server!');
+
+});
+
+
+app.get('*', (request, response) => {
+  response.status(404).send('Not availabe');
+});
+
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
