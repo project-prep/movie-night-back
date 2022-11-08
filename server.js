4909fd55a92c78b00ee209457ddf3498625b3c75
Jordan <jkwan104@gmail.com>
2022-11-07 16:13:50 -0800

    skeleton

--- server.js
+++ server.js
@@ -2,17 +2,18 @@
 
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_KEY);
+// mongoose.connect(process.env.DB_KEY);
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('Mongoose is connected');
-});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//   console.log('Mongoose is connected');
+// });
 
 const app = express();
 app.use(cors());
@@ -25,6 +26,34 @@ app.get('/', (request, response) => {
   response.status(200).send('Welcome to Our Server!');
 
 });
+
+app.get('/movies', async (request, response, next) => {
+    try {
+
+        let title = request.query.title;
+
+        let url = `https://ott-details.p.rapidapi.com/search?title=${title}&rapidapi-key=${process.env.MOVIE_KEY})`
+
+        let getMovie = await axios.get(url);
+
+        console.log('results', getMovie.data);
+
+        let groomedData = getMovie.data.results.map(movie => {return new Film(movie)});
+            response.status(200).send(groomedData);
+    } catch (error) {
+        next(error);
+    }
+
+});
+
+class Film{
+    constructor(films) {
+        this.title = films.title;
+        this.synopsis = films.synopsis;
+        this.released = films.released;
+        this.imageurl = films.imageurl;
+    }
+}
 
 
 app.get('*', (request, response) => {
